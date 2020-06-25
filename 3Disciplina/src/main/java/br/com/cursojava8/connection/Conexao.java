6c485bf042c585e0991abab5b5bc2eaea81ac474
pablo gonzalez <pablo949504563@gmail.com>
2020-06-25 20:27:39 -0300

    Conexão com banco de dados

---
+++ 3Disciplina/src/main/java/br/com/cursojava8/connection/Conexao.java
@@ -0,0 +1,65 @@
+package br.com.cursojava8.connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
+public class Conexao {
+	
+	private static Connection conexao;
+	
+	private static final String URL_CONEXAO = "jdbc:mysql://localhost/curso-java?useTimezone=true&serverTimezone=UTC";
+	private static final String USUARIO = "root";
+	private static final String SENHA = "!ndr@AdmR0b0#2019";
+	
+	
+	public static Connection getConexao() {
+		if (conexao == null) {
+			try {
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					conexao = (Connection) DriverManager.getConnection(URL_CONEXAO, USUARIO, SENHA);
+				} catch (ClassNotFoundException e) {
+
+					e.printStackTrace();
+				}
+
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+
+		}
+		return conexao;
+	}
+public static void fecharConexao(){
+	if(conexao != null){
+		try {
+			conexao.close();
+			conexao = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}	
+	
+public static void main(String []args) {
+	getConexao();
+	try {
+		conexao.getLog();
+		System.out.println("conectou");
+		
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	fecharConexao();
+	
+	
+}
+
+
+
+}
